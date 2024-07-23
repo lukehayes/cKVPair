e159a5acdf830f2d958476d2fb3cfebf13d33e6b
lukehayes <lukepurplehayes@gmail.com>
2024-07-23 09:28:48 +0100

    Added cStr library to project

--- include/str.h
+++ include/str.h
@@ -6,17 +6,42 @@
 typedef struct Str
 {
     char* text;
-    size_t size;
-
+    size_t length;
 } Str;
 
-Str* ldh_StrCreate(const char* s)
-{
+/**
+* Create a new Str object.
+*
+* @param const char* s    The text for the Str object;
+*
+* @return Str*    A new Str object.
+*/
+Str* ldh_StrCreate(const char* s);
+
+/**
+* Destroy a Str object.
+*
+* @param Str*    The Str object to be destroyed.
+*/
+void ldh_StrDestroy(Str* s);
+
+/**
+* Reverse a the string contents of a Str object.
+*
+* @param Str*    The Str object to be destroyed.
+*/
+void ldh_StrRev(Str* s);
+
+/**
+* Print a Str object to std out.
+*
+* @param Str*    The Str object to be destroyed.
+*/
+void ldh_StrPrint(Str* s);
 
 
-}
+
+#endif // LDH_STR_H
 
 
 
-
-#endif // !LDH_STR_H
