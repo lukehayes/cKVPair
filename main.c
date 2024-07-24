a3776c277676e1da09bc40051d9acc23da3c0b68
lukehayes <lukepurplehayes@gmail.com>
2024-07-24 09:57:51 +0100

    Updated main

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
 #include "io.h"
-#include "str.h"
 #include "kvpair.h"
-#include <string.h>
+#include "kvmap.h"
+#include <stdio.h>
 
-
-void resetMemory(char* mem, size_t size)
-{
-    for(int i = 0; i <= size - 1; i++)
-    {
-        mem[i] = 0;
-    }
-}
 
 int main()
 {
-    struct BufferData* buffer = ReadFileIntoBuffer("data.toml");
+    KVPair* p = ldh_KVPairCreate("Hello", "A Value");
 
-    KVPair* p = ldh_KVPairCreate("...", "A Value");
+    KVMap* map = ldh_KVMapCreate(10);
 
-    printf("Key: %s, %lu Value: %s %lu\n", p->key->text, p->key->length, p->value->text, p->value->length);
-
+    ldh_KVMapDestroy(map);
     ldh_KVPairDestroy(p);
 
     return 0;
