56ce90a875287cb4fe3728c304a0f7a0c34d515a
lukehayes <lukepurplehayes@gmail.com>
2024-07-24 09:57:00 +0100

    Added ldh_KVMapCreate() and ldh_KVMapDestroy()

--- include/kvmap.h
+++ include/kvmap.h
@@ -1,6 +1,33 @@
 #ifndef LDH_KVMAP_H
 #define LDH_KVMAP_H
 
+#include "kvpair.h"
+
+typedef struct KVMap
+{
+    KVPair** list;
+    size_t capacity;
+    size_t size;
+} KVMap;
+
+
+/**
+ * Create a KVMap instance.
+ *
+ * @param size_t capacity    The initial size of the map.
+ *
+ * @return KVMap*
+ */
+KVMap* ldh_KVMapCreate(size_t capacity);
+
+/**
+ * Free the KVMap from memory.
+ *
+ * @param KVMap* map    The pointer to be freed.
+ */
+void ldh_KVMapDestroy(KVMap* map);
+
+
 /**
  * A hashing function - all credit goes to: http://www.cse.yorku.ca/~oz/hash.html
  *
--- src/kvmap.c
+++ src/kvmap.c
@@ -1,5 +1,22 @@
 #include "kvmap.h"
 
+#include <stdlib.h>
+
+KVMap* ldh_KVMapCreate(size_t capacity)
+{
+    KVMap* map = malloc(sizeof(KVMap));
+    map->capacity = capacity;
+    map->size = 0;
+
+    map->list = malloc(sizeof(KVMap) * capacity);
+    return map;
+}
+
+void ldh_KVMapDestroy(KVMap* map)
+{
+    free(map->list);
+    free(map);
+}
 
 unsigned long
 ldh_Hash(unsigned char *str)
