31dce97308690169114550646a5b39445ffc173f
lukehayes <lukepurplehayes@gmail.com>
2024-10-21 11:29:07 +0100

    Refactored parsing logic and null chars added correctly

--- main.c
+++ main.c
@@ -29,16 +29,15 @@ void PB(char* buf)
     printf("Buf: %s \n", buf);
 }
 
-bool NotPunctOrSpace(const char* c)
+void PS(const char* s)
 {
-    return (!ispunct(*c) || !isspace(*c)) ? 1 : 0;
+    printf("Str: %s\n", s);
 }
 
 bool IsValidChar(const char* c)
 {
     return (!ispunct(*c) || !isspace(*c)) ? 1 : 0;
 }
-
 
 char* NextValidChar(char* c)
 {
@@ -54,19 +53,67 @@ char* NextValidChar(char* c)
 
 void ResetBuffer(char* buffer, size_t size)
 {
-    printf("Reseting...\n");
     memset(buffer, '0', size);
+    printf("Reset Buffer...\n");
+}
 
-    /*for (int i = 0; i <= size -1 ; i++)*/
-    /*{*/
-        /*buffer[i] = 0;*/
-    /*}*/
+char* ParseValue(char* c, char* property)
+{
+
+    char* ptr = c;
+
+
+    int bufSize = 100;
+    char buffer[bufSize];
+    int memIndex = 0;
+
+    while(isalnum(*ptr))
+    {
+        buffer[memIndex] = *ptr;
+        memIndex++;
+        ptr++;
+
+        // Pointer already advanced so no need to ptr++ it.
+        // It checks the next value in the list.
+        // So add the NULL char.
+        if (isspace(*ptr)) 
+        {
+            // Increased memIndex by one to make space for NULL char.
+            memIndex++;
+
+            buffer[memIndex] = '\0';
+            property[memIndex] = '\0';
+            
+            if (iscntrl(buffer[memIndex])) 
+            {
+                memIndex--;
+                memset(property, '0', memIndex);
+                memcpy(property, buffer, sizeof(char) * (memIndex));
+            }else 
+            {
+                memset(property, '0', memIndex);
+                memcpy(property, buffer, sizeof(char) * memIndex);
+            }
+
+
+            break;
+        }
+    }
+
+    PB(buffer);
+    ResetBuffer(buffer, memIndex);
+    return ptr;
+}
+
+void AdvancePtr(char* charPtr)
+{
+
 }
 
 
 typedef struct Pair {
-    char* key;
-    char* value;
+    char key[30];
+    char value[30];
 } Pair;
 
 
@@ -74,115 +121,34 @@ int main()
 {
     BufferData* buffer = ReadFileIntoBuffer("data.txt");
 
-    int bufSize = 20;
-    int keyCounter = 1;
-    char* keyBuffer;
     char* currentChar = buffer->data;
-    int lineCount = 0;
 
-    Pair* p  = malloc(sizeof(Pair));
-    p->key   = malloc(sizeof(char) * bufSize);
-    p->value = malloc(sizeof(char) * bufSize);
+    Pair p;
 
     while(*currentChar)
     {
-        char* ptr = currentChar;
-        static int memIndex = 0;
-        char buffer[bufSize];
-
-
-        if (isalnum(*ptr))
+        // Parse Key
+        currentChar = ParseValue(currentChar, p.key);
+        while ( ispunct(*currentChar) != 0 || isspace(*currentChar) != 0) 
         {
-            printf("Parsing key...\n");
-
-            // Parse Key
-            // Key can only be alpha so no need to check for alpha nums
-            while(isalnum(*ptr))
-            {
-                buffer[memIndex] = *ptr;
-                memIndex++;
-                ptr++;
-
-                // Pointer already advanced so no need to ptr++ it.
-                // It checks the next value in the list.
-                // So add the NULL char.
-                if (isspace(*ptr)) 
-                {
-                    buffer[memIndex] = '\0';
-
-                    memset(p->key, '0', memIndex);
-                    p->key[memIndex] = '\0';
-
-                    memcpy(p->key, buffer, sizeof(char) * memIndex);
-                    break;
-                }
-            }
-
-            while ( ispunct(*ptr) != 0 || isspace(*ptr) != 0) 
-            {
-                printf("Moving to next valid char... %c\n", *ptr);
-                ptr++;
-            }
-
-            printf("Setting current char to %c\n", *ptr);
-
-            ResetBuffer(buffer, bufSize);
-            memIndex = 0;
-
-
-            // Parse Value
-            printf("Parsing key...\n");
-
-            while(isalnum(*ptr))
-            {
-                buffer[memIndex] = *ptr;
-                memIndex++;
-                ptr++;
-
-                // Pointer already advanced so no need to ptr++ it.
-                // It checks the next value in the list.
-                // So add the NULL char.
-                if (isspace(*ptr)) 
-                {
-                    buffer[memIndex] = '\0';
-                    PI(memIndex);
-
-                    memset(p->value, '0', memIndex);
-                    p->value[memIndex] = '\0';
-                    memcpy(p->value, buffer, sizeof(char) * memIndex);
-                    break;
-                }
-            }
-
-            
-            // Set the current char to 1 before the valid one.
-            currentChar = ptr;
+            printf("Moving to next valid char... %c\n", *currentChar);
+            currentChar++;
         }
 
-        printf("NOW CURR CHAR to %c\n", *currentChar);
-        printf("NOW  PTR to %c\n", *ptr );
-
-
-        // Reached end of line.
-        if (IsNewLine(currentChar)) {
-            lineCount++;
-        }
+        // Parse Value
+        currentChar = ParseValue(currentChar, p.value);
 
         currentChar++;
     }
 
 
-    printf("K:%s V:%s \n", p->key, p->value);
+    printf("K:%s V:%s\n", p.key, p.value);
 
-    int health = (int)p->value;
-    printf("Health %lu\n", (int)health);
-
-    printf("Lines: %i \n", lineCount);
     DestroyBufferData(buffer);
 
-    free(p->key);
-    free(p->value);
-    free(p);
+    //free(p->key);
+    //free(p->value);
+    //free(p);
 
     return 0;
 }
