6c9722d68d3cb6feb96cc6266ea8f1cb2a6c2c9c
lukehayes <lukepurplehayes@gmail.com>
2024-10-22 08:39:56 +0100

    Added print helpers for float and double types

--- include/print.h
+++ include/print.h
@@ -1,9 +1,7 @@
 #ifndef LDH_PRINT_H
 #define LDH_PRINT_H
 
-
 #include <stdio.h>
-
 
 // Helper functions for printing.
 
@@ -15,6 +13,16 @@ void PC(char* c)
 void PI(int i)
 {
     printf("Int: %i \n", i);
+}
+
+void PF(float f)
+{
+    printf("Float: %f \n", f);
+}
+
+void PD(double d)
+{
+    printf("Double: %f \n", d);
 }
 
 void PB(char* buf)
