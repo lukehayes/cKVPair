4a21cb46aca02a420c6233b3560a6f2609e4a0bd
lukehayes <lukepurplehayes@gmail.com>
2024-10-25 17:12:11 +0100

    Moved parsing logic into parser.h

--- include/parser.h
+++ include/parser.h
@@ -2,6 +2,9 @@
 #define LDH_PARSER_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "print.h"
 
 typedef enum ValueType
 {
@@ -16,5 +19,129 @@ typedef struct Value {
     ValueType type;
 } Value;
 
+
+/**
+ * The parser uses an internal temporary buffer, this function resets it.
+ *
+ * @param char* buffer.
+ * @param size_t size.
+ */
+void ResetBuffer(char* buffer, size_t size)
+{
+    memset(buffer, '0', size);
+}
+
+/**
+* Parse the a single key or value from a string.
+*
+* @param Value* val.
+* @param sizt_t memIndex.
+* @param char*  buffer.
+*/
+void ParseValueType(Value* val, int memIndex, char* buffer)
+{
+    // Add basic type information about the value here.
+    // The type can either be STRING or INT at the moment/
+    if (isalpha(*buffer))
+    {
+        val->type = STRING;
+    }
+
+    if (isdigit(*buffer))
+    {
+        val->type = INT;
+    }
+
+
+    // Allocate the space for the value here and add it to the buffer
+    switch (val->type) {
+        case STRING:
+            val->value = (char*)malloc(sizeof(char) * memIndex);
+            memset(val->value, '0', memIndex);
+            memcpy(val->value, buffer, sizeof(char) * memIndex);
+
+            val->length = memIndex;
+            char* k = (char*)val->value;
+
+            k[val->length] = '\0';
+
+            break;
+
+        case INT:
+            val->value = (int*)malloc(sizeof(int) * memIndex);
+            memset(val->value, '0', memIndex);
+            memcpy(val->value, buffer, sizeof(int) * memIndex);
+
+            val->length = memIndex;
+            int* v = (int*)val->value;
+
+            v[val->length] = '\0';
+
+            break;
+        default:
+    }
+}
+
+/**
+ * Advace the current pointer to the next valid char.
+ * @param char* charPtr.
+ * @return char*
+ */
+char* AdvancePtr(char* charPtr)
+{
+    while ( ispunct(*charPtr) != 0 || isspace(*charPtr) != 0)
+    {
+        /*printf("Moving to next valid char... %c\n", *charPtr);*/
+        charPtr++;
+    }
+
+    return charPtr;
+}
+
+/**
+* Parse a string for key value pairs
+*
+* @param char* c.
+* @param Value* val.
+*/
+char* ParseValue(char* c, Value* val)
+{
+    char* ptr = c;
+
+    int bufSize = 10;
+    char buffer[bufSize];
+    int memIndex = 0;
+
+    while(isalnum(*ptr))
+    {
+        buffer[memIndex] = *ptr;
+        memIndex++;
+        ptr++;
+
+        // Pointer already advanced so no need to ptr++ it.
+        if (isspace(*ptr))
+        {
+            // Increased memIndex by one to make space for NULL char.
+            memIndex++;
+
+            buffer[memIndex - 1] = '\0';
+
+            if (iscntrl(buffer[memIndex]))
+            {
+                memIndex--;
+
+                ParseValueType(val, memIndex, buffer);
+
+            }else
+            {
+                ParseValueType(val, memIndex, buffer);
+            }
+            break;
+        }
+    }
+
+    ResetBuffer(buffer, memIndex);
+    return ptr;
+}
 
 #endif // !LDH_PARSER_H
--- main.c
+++ main.c
@@ -2,142 +2,10 @@
 #include "io.h"
 #include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
 #include "print.h"
 #include "parser.h"
 
-bool IsNewLine(const char* c)
-{
-    return *c == '\n';
-}
-
-void NewLine()
-{
-    printf("\n");
-}
-
-
-bool IsValidChar(const char* c)
-{
-    return (!ispunct(*c) || !isspace(*c)) ? 1 : 0;
-}
-
-char* NextValidChar(char* c)
-{
-    char* ptr = c;
-
-    while(!isalnum(ptr))
-    {
-        ptr++;
-    }
-
-    return ptr;
-}
-
-void ResetBuffer(char* buffer, size_t size)
-{
-    memset(buffer, '0', size);
-    /*printf("Reset Buffer...\n");*/
-}
-
-void ParseValueType(Value* val, int memIndex, char* buffer)
-{
-    if (isalpha(*buffer))
-    {
-        val->type = STRING;
-    }
-
-    if (isdigit(*buffer))
-    {
-        val->type = INT;
-    }
-
-
-    switch (val->type) {
-        case STRING:
-            val->value = (char*)malloc(sizeof(char) * memIndex);
-            memset(val->value, '0', memIndex);
-            memcpy(val->value, buffer, sizeof(char) * memIndex);
-
-            val->length = memIndex;
-            char* k = (char*)val->value;
-
-            k[val->length] = '\0';
-
-            break;
-
-        case INT:
-            PS("Storing Int");
-            val->value = (int*)malloc(sizeof(int) * memIndex);
-            memset(val->value, '0', memIndex);
-            memcpy(val->value, buffer, sizeof(int) * memIndex);
-
-            val->length = memIndex;
-            int* v = (int*)val->value;
-
-            v[val->length] = '\0';
-
-            break;
-        default:
-    }
-}
-
-
-char* ParseValue(char* c, Value* property)
-{
-
-    char* ptr = c;
-
-
-    int bufSize = 100;
-    char buffer[bufSize];
-    int memIndex = 0;
-
-    while(isalnum(*ptr))
-    {
-        buffer[memIndex] = *ptr;
-        memIndex++;
-        ptr++;
-
-        // Pointer already advanced so no need to ptr++ it.
-        // It checks the next value in the list.
-        // So add the NULL char.
-        if (isspace(*ptr))
-        {
-            // Increased memIndex by one to make space for NULL char.
-            memIndex++;
-
-            buffer[memIndex - 1] = '\0';
-
-            if (iscntrl(buffer[memIndex]))
-            {
-                memIndex--;
-
-                ParseValueType(property, memIndex, buffer);
-
-            }else
-            {
-                ParseValueType(property, memIndex, buffer);
-            }
-            break;
-        }
-    }
-
-    PB(buffer);
-    ResetBuffer(buffer, memIndex);
-    return ptr;
-}
-
-char* AdvancePtr(char* charPtr)
-{
-    while ( ispunct(*charPtr) != 0 || isspace(*charPtr) != 0)
-    {
-        /*printf("Moving to next valid char... %c\n", *charPtr);*/
-        charPtr++;
-    }
-
-    return charPtr;
-}
+#define DerefTypedValk
 
 int main()
 {
@@ -145,26 +13,26 @@ int main()
 
     char* currentChar = buffer->data;
 
-    Value k;
-    Value v;
+    Value val[8];
 
     while(*currentChar)
     {
         // Parse Key
-        currentChar = ParseValue(currentChar, &k);
+        currentChar = ParseValue(currentChar, &val[0]);
 
         currentChar = AdvancePtr(currentChar);
         // Parse Value
-        currentChar = ParseValue(currentChar, &v);
+        currentChar = ParseValue(currentChar, &val[1]);
 
+
+        printf("Key %s | Val %s \n", val[0].value, val[1].value);
 
         currentChar++;
     }
 
 
-    printf("K:%s V:%s\n", k.value, v.value);
 
-    free(k.value);
+    /*printf("K:%s V:%s T:%lu\n", k.value, ((int*)v.value), atol(v.value) + 99 * 5 + 1);*/
 
     DestroyBufferData(buffer);
 
